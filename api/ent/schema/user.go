d93f925856b1d1c782868c5496e3195efc7c47da
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-08-08 13:05:11 +0700

    add schema model database

---
+++ api/ent/schema/user.go
@@ -0,0 +1,42 @@
+package schema
+
+import (
+	"github.com/facebookincubator/ent"
+	"github.com/facebookincubator/ent/schema/field"
+	"github.com/facebookincubator/ent/schema/index"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Indexes hold index field of user entity
+func (User) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("email", "password").
+			Unique(),
+	}
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("firstname").
+			NotEmpty(),
+		field.String("lastname").
+			NotEmpty(),
+		field.String("email").
+			NotEmpty().
+			Unique(),
+		field.String("phonenumber").
+			NotEmpty(),
+		field.String("password").
+			NotEmpty(),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return nil
+}
