c1cccd64d6abd0ddabcd8409fda726fb917e58dc
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-08-24 11:24:37 +0700

    add main to run graphql server

--- api/main.go
+++ api/main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/awesomebusiness/uinvest/config"
+	"github.com/awesomebusiness/uinvest/internal/generated"
+	"github.com/awesomebusiness/uinvest/internal/resolver"
+	"github.com/awesomebusiness/uinvest/internal/service/authentication/repository"
+	"github.com/awesomebusiness/uinvest/internal/service/authentication/usecase"
+	"github.com/joho/godotenv"
+)
+
+const defaultPort = "8080"
 
 func main() {
-	fmt.Println("U-Invest Application On Going!!!")
+	ctx := context.Background()
+
+	// load environment variable from .env
+	_ = godotenv.Load()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	db, err := config.NewDatabase()
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+	defer db.Client.Close()
+
+	if err := db.Client.Schema.Create(ctx); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	authrepo := repository.NewAuthenticationRepository(db.Client)
+	authuc := usecase.NewAuthenticationUsecase(authrepo)
+
+	config := generated.Config{
+		Resolvers: &resolver.Resolver{
+			AuthenticationUC: authuc,
+		},
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(config))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
