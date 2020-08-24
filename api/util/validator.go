e0a39a756bb38eae26cabeb323196e6e900fd794
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-08-24 12:54:34 +0700

    add util for validator

---
+++ api/util/validator.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"regexp"
+)
+
+// IsEmailValid is validation to check email valid or not and return boolean
+func IsEmailValid(email string) bool {
+	emailRegex := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+	if email == "" || !emailRegex.MatchString(email) {
+		return false
+	}
+
+	return true
+}
+
+// IsPhoneNumberValid is validation to check whether phone number valid or not and return boolean
+func IsPhoneNumberValid(phonenumber string) bool {
+	phonenumberChar := string([]rune(phonenumber)[0]) + string([]rune(phonenumber)[1])
+
+	if phonenumber == "" || len(phonenumber) < 12 || phonenumberChar != "62" {
+		return false
+	}
+
+	return true
+}
